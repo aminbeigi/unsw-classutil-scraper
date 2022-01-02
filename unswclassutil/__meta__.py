7789798ca7c81d153eb85ffbab3ce2e155f8932b
aminbeigi <me@aminbeigi.com>
2022-01-02 16:30:51 +1100

    Package modules

---
+++ unswclassutil/__meta__.py
@@ -0,0 +1,12 @@
+__author__ = 'Amin Ghasembeigi'
+__description__ = 'A web scraper.'
+__email__ = 'me@aminbeigi.com'
+__license__ = "MIT"
+__maintainer__ = 'Amin Ghasembeigi'
+__status__ = 'Development'
+__title__ = 'unswclassutil'
+__website__ = 'https://github.com/aminbeigi/unsw-classutil-scraper'
+__version__ = '0.0.1'
+
+# make metadata public to script
+__all__ = ['__author__', '__description__', '__email__', '__license__', '__maintainer__', '__status__', '__title__', '__website__', '__version__']
