c03bf939dbdb273e94c8fa9c6cdc58d3fb1e3067
aminbeigi <me@aminbeigi.com>
2022-01-03 16:57:15 +1100

    Remove print statement

--- unswclassutil/webscraper.py
+++ unswclassutil/webscraper.py
@@ -10,7 +10,6 @@ class WebScraper:
     @staticmethod
     def browser(term: str) -> mechanicalsoup.StatefulBrowser:
         browser = mechanicalsoup.StatefulBrowser()
-        print("###", type(browser))
         response = browser.open(WebScraper._URL)
         if response.status_code != HTTPStatus.OK:
             raise HTTPError(f'Expected status code 200 but actual was {response.status_code}.')
